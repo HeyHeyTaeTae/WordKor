e67fc1d76e7ebf11c7ee09fd43f8ecaadd9d45e4
Tae Kim <taekimsf@gmail.com>
2017-07-02 13:56:54 -0700

    added date display to HTML and JS and styled it to match word elements in CSS

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 window.onload = function() {
 	console.log("Santity check!");
+	var dateCont = document.querySelector("#date");
+	console.log(dateCont);
+	dateCont.innerHTML = new Date().toDateString();
 	var wordCont = document.querySelector(".word-h1");
 	var randWord = wordList[Math.floor(Math.random() * wordList.length)];
 	wordCont.innerHTML = randWord;
