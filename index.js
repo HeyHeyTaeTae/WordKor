fdafcc770c758b904f7a8da7115d2c4fa44af6e6
Tae Kim <taekimsf@gmail.com>
2017-07-02 14:30:26 -0700

    converted wordList to array of objects with korean word and translation

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 window.onload = function() {
 	console.log("Santity check!");
 	var dateCont = document.querySelector("#date");
-	console.log(dateCont);
 	dateCont.innerHTML = new Date().toDateString();
 	var wordCont = document.querySelector(".word-h1");
 	var randWord = wordList[Math.floor(Math.random() * wordList.length)];
@@ -10,5 +9,26 @@ window.onload = function() {
 
 
 
-var wordList = ["자기", "사랑", "커피", "고양이", "뽀뽀", "딸기", "학교", "강아지", "친구", "사탕", "설탕"];
+var wordList = [{korean:"자기",
+				translation:"Darling"},
+				{korean:"사랑",
+				translation:"Love"},
+				{korean:"커피",
+				translation:"Coffee"},
+				{korean:"고양이",
+				translation:"Cat"},
+				{korean:"뽀뽀",
+				translation:"Kiss"},
+				{korean:"딸기",
+				translation:"Strawberry"},
+				{korean:"학교",
+				translation:"School"},
+				{korean:"강아지",
+				translation:"Puppy"},
+				{korean:"친구",
+				translation:"Friend"},
+				{korean:"사탕",
+				translation:"Candy"},
+				{korean:"설탕",
+				translation:"Sugar"];
 
