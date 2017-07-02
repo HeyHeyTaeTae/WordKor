3a7d1168f4a381fcdc3a6797609b96619dd716cc
Tae Kim <taekimsf@gmail.com>
2017-07-02 13:04:20 -0700

    added array of dummy words and random innerHTML on pageload. MVP achieved

---
+++ index.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+	console.log("Santity check!");
+	var wordCont = document.querySelector(".word-h1");
+	var randWord = wordList[Math.floor(Math.random() * wordList.length)];
+	wordCont.innerHTML = randWord;
+};
+
+
+
+var wordList = ["자기", "사랑", "커피", "고양이", "뽀뽀", "딸기", "학교", "강아지", "친구"];
+
