0446d14fbb37b4bab41507a555f7d71b61c59c9b
Tae Kim <taekimsf@gmail.com>
2017-07-03 09:16:32 -0700

    minor changes to wordList

--- index.js
+++ index.js
@@ -20,37 +20,49 @@ window.onload = function() {
 	};
 };
 
-var wordList = [{korean:"자기",
+var wordList = [
+				{korean:"자기",
 				translation:"Darling",
 				romanization:"(Jya-gi)"},
+
 				{korean:"사랑",
 				translation:"Love",
 				romanization:"(Sa-rahng)"},
+
 				{korean:"커피",
 				translation:"Coffee",
 				romanization:"(Kuh-pee)"},
+
 				{korean:"고양이",
 				translation:"Cat",
 				romanization:"(Goh-yahng-ee)"},
+
 				{korean:"뽀뽀",
 				translation:"Kiss",
 				romanization:"(Bboh-bboh)"},
+
 				{korean:"딸기",
 				translation:"Strawberry",
 				romanization:"(Thahl-gi)"},
+
 				{korean:"학교",
 				translation:"School",
 				romanization:"(Hahk-giyo)"},
+
 				{korean:"강아지",
 				translation:"Puppy",
 				romanization:"(Gahng-ah-gee)"},
+
 				{korean:"친구",
 				translation:"Friend",
 				romanization:"(Ching-goo)"},
+
 				{korean:"사탕",
 				translation:"Candy",
 				romanization:"(Sah-tahng)"},
+
 				{korean:"설탕",
 				translation:"Sugar",
-				romanization:"(Suhl-tahng)"}];
+				romanization:"(Seohl-tahng)"}
+				];
 
