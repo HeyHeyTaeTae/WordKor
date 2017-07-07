2e1f8eefc74a957343900bc33e0042856fa4fcb4
Tae Kim <taekimsf@gmail.com>
2017-07-07 16:50:52 -0700

    moved click event for translation and romanization as callback function to setWord function

--- index.js
+++ index.js
@@ -6,44 +6,10 @@ window.onload = function() {
 	dateCont.innerHTML = new Date().toDateString();
 
 	// grab containers for word info
-	var wordCont = document.querySelector(".word-h1");
-	var romanCont = document.querySelector(".romanization-h1");
-	var englishTranslBtn = document.querySelector(".english-translation-button");
-	var romanBtn = document.querySelector(".romanization-button");
+	
 	var categoryBtn = document.querySelector(".category-button");
 	var categoryDrop = document.querySelector(".category-dropmenu");
 	var foodCategory = document.querySelector(".food-category");
-
-	// grab random word from array
-	var randWordObj = vocabList[Math.floor(Math.random() * vocabList.length)];
-	var randWordKor = randWordObj.korean;
-	var randWordTransl = randWordObj.translation;
-	var randWordRoman = randWordObj.romanization;
-	wordCont.innerHTML = randWordKor;
-
-	// grab random word from food array
-	var randFoodWord = foodList[Math.floor(Math.random() * foodList.length)];
-	var randFoodKor = randFoodWord.korean;
-
-
-	
-	// click toggle for romanization
-	romanBtn.onclick = function() {
-		if(romanCont.innerHTML === randWordRoman) {
-			romanCont.innerHTML = "";
-		} else {
-			romanCont.innerHTML = randWordRoman;
-		}
-	};
-
-	// click toggle for Korean -> English
-	englishTranslBtn.onclick = function() {
-		if(wordCont.innerHTML === randWordKor) {
-			wordCont.innerHTML = randWordTransl;	
-		} else {
-			wordCont.innerHTML = randWordKor;
-		}
-	};
 
 	// click hide/show for category drop
 	categoryBtn.onclick = function() {
@@ -56,119 +22,162 @@ window.onload = function() {
 		}
 	};
 
-	// click render for food category
-	foodCategory.onclick = function() {
-		wordCont.innerHTML = randFoodKor;
+	// click event propagation for category ul
+	// categoryDrop.onclick = function() {
+	// 	var category = this.innerHTML;
+	// 	for(word=0; word<vocabList.length; word++) {
+	// 		var categoryVocabList = [];
+	// 		if(word.category === category) {
+	// 			categoryVocabList.push(word);
+	// 		} else {
+	// 			word++
+	// 		}
+	// 	}
+
+	// }
+
+	// sets word in word container
+	var setWord = function(array, callback) {
+		var wordCont = document.querySelector(".word-h1");
+		var word = array[Math.floor(Math.random() * array.length)];
+		var wordKor = word.korean;
+		wordCont.innerHTML = wordKor;
+		// invoke callback to set click events for buttons
+		callback(word, wordCont);
+	};
+
+	var setBtnEvent = function(word, container) {
+		var englishTranslBtn = document.querySelector(".english-translation-button");
+		var romanBtn = document.querySelector(".romanization-button");
+		console.log(word);
+		// click toggle for romanization
+		var romanCont = document.querySelector(".romanization-h1");
+		romanBtn.onclick = function() {
+			if(romanCont.innerHTML === word.romanization) {
+				romanCont.innerHTML = "";
+			} else {
+				romanCont.innerHTML = word.romanization;
+			}
+		};
+
+		// click toggle for Korean -> English
+		englishTranslBtn.onclick = function() {
+			console.log(container);
+			if(container.innerHTML === word.korean) {
+				container.innerHTML = word.translation;	
+			} else {
+				container.innerHTML = word.korean;
+			}
+		};
 	}
-
-
+	setWord(vocabList, setBtnEvent);
 };
 
 var vocabList = [
 				{korean:"자기",
 				translation:"Darling",
 				romanization:"(Jya-gi)",
-				category:"relationship"},
+				category:"Relationship"},
 
 				{korean:"사랑",
 				translation:"Love",
 				romanization:"(Sa-rahng)",
-				category:"relationship"},
+				category:"Relationship"},
 
 				{korean:"커피",
 				translation:"Coffee",
 				romanization:"(Kuh-pee)",
-				category:"food"},
+				category:"Food"},
 
 				{korean:"고양이",
 				translation:"Cat",
 				romanization:"(Goh-yahng-ee)",
-				category:"animals"},
+				category:"Animals"},
 
 				{korean:"뽀뽀",
 				translation:"Kiss",
 				romanization:"(Bboh-bboh)",
-				category:"relationship"},
+				category:"Relationship"},
 
 				{korean:"딸기",
 				translation:"Strawberry",
 				romanization:"(Thahl-gi)",
-				category:"food"},
+				category:"Food"},
 
 				{korean:"학교",
 				translation:"School",
 				romanization:"(Hahk-giyo)",
-				category:"education"},
+				category:"Education"},
 
 				{korean:"강아지",
 				translation:"Puppy",
 				romanization:"(Gahng-ah-gee)",
-				category:"animals"},
+				category:"Animals"},
 
 				{korean:"친구",
 				translation:"Friend",
 				romanization:"(Ching-goo)",
-				category:"relationship"},
+				category:"Relationship"},
 
 				{korean:"사탕",
 				translation:"Candy",
 				romanization:"(Sah-tahng)",
-				category:"food"},
+				category:"Food"},
 
 				{korean:"설탕",
 				translation:"Sugar",
 				romanization:"(Seohl-tahng)",
-				category:"food"},
+				category:"Food"},
 				
 				{korean:"사과",
 				translation:"Apple",
 				romanization:"(Sah-guah)",
-				category:"food"},
+				category:"Food"},
 
 				{korean:"소고기",
 				translation:"Beef",
 				romanization:"(Soh-goh-gi)",
-				category:"food"},
+				category:"Food"},
 
 				{korean:"양파",
 				translation:"Onion",
 				romanization:"(Yahng-pah)",
-				category:"food"},
+				category:"Food"},
 
 				{korean:"마늘",
 				translation:"Garlic",
 				romanization:"(Mah-neul)",
-				category:"food"},
+				category:"Food"},
 
 				{korean:"파",
 				translation:"Green Onion",
 				romanization:"(Pah)",
-				category:"food"},
+				category:"Food"},
 
 				{korean:"두부",
 				translation:"Tofu",
 				romanization:"(Doo-boo)",
-				category:"food"},
+				category:"Food"},
 
 				{korean:"우유",
 				translation:"Milk",
 				romanization:"(Ooo-yoo)",
-				category:"food"},
+				category:"Food"},
 
 				{korean:"만두",
 				translation:"Dumplings",
 				romanization:"(Mahn-doo)",
-				category:"food"},
+				category:"Food"},
 
 				{korean:"김치",
 				translation:"Kimchi",
 				romanization:"(Gim-chee)",
-				category:"food"},
+				category:"Food"},
 
 				{korean:"밥",
 				translation:"Rice",
 				romanization:"(Bahp)",
-				category:"food"}
+				category:"Food"}
 			    ]
 
 
